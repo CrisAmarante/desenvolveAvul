49b039d230b27c42531c1fb78e62aa2396580d6c
Cris Amarante <amarante_s@hotmail.com>
2026-04-02 18:58:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // ====================================================================
 // CONFIGURAÇÕES GERAIS
 // ====================================================================
-const URL_PLANILHA = "https://script.google.com/macros/s/AKfycbzM47z9njqsKW5BT2OiKq9nGKwZrgrrbMI4F4JTi1oJzd2xDAvXtCSvBH-C_4-VlO6K/exec";
 
 let INSPETORES = {};
 
