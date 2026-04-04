90c9dd24183645c21e582579571402cdc6b632c5
Cris Amarante <amarante_s@hotmail.com>
2026-04-04 15:31:23 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const URL_PLANILHA = "https://script.google.com/macros/s/AKfycbzJqi0uxx4VcHxbvhxxnjujLnsdOJJR5OqyYCuKOXfTLxBjimhytcBmws7nT-mdsP4Q/exec";
+const URL_PLANILHA = "https://script.google.com/macros/s/AKfycbyeEM5xCe8VHLcXATr_JdVFJlP8QiXNVLHqKE_6f4aiUUY7gvBk8eyBFMs9X9-ES1sK/exec";
