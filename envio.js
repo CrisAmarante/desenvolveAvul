e25e3a59046cb1f0bf9b97ec67d48549b816c7e0
Cris Amarante <amarante_s@hotmail.com>
2026-04-07 06:24:55 -0300

    Update envio.js

--- envio.js
+++ envio.js
@@ -8,18 +8,26 @@ let anexosArray = [];          // cada elemento: { base64, mimeType, nome }
 function getEl(id) { return document.getElementById(id); }
 
 // --- Abrir/fechar modal ---
-function abrirModalEnvio() {
+async function abrirModalEnvio() {
   const m = getEl('modal-envio-informacoes');
   if (m) m.classList.add('is-open');
+  
+  // Garante que os terminais estejam carregados antes de preencher o select
+  if (!window.terminais || window.terminais.length === 0) {
+    if (typeof carregarTerminais === 'function') {
+      await carregarTerminais(true);
+    }
+  }
+  
   preencherDataAtual();
   preencherResponsavel();
-  preencherSelectLocal();
+  preencherSelectLocal();    // agora os terminais já existem
   carregarRascunho();
   habilitarCamposSecundarios(false);
   anexosArray = [];
   atualizarListaAnexos();
   const input = getEl('input-arquivos');
-  if(input) input.value = ''; // Zera o input real
+  if(input) input.value = '';
 }
 
 function fecharModalEnvio() {
