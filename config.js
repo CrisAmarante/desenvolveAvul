40de3f591a2f55fe01ee9bf21f8b864c18f0868b
Cris Amarante <amarante_s@hotmail.com>
2026-04-17 06:35:36 -0300

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,4 @@
 
 //=======================COMENTE SE NÃO ESTIVER EM USO====REMOVA O COMENTÁRIO SE FOR USAR=================================================
 //ambiente de testes de campo
-const URL_PLANILHA = "https://script.google.com/macros/s/AKfycbz004U9xi4C610xCWqGAMPZ8Mb3NlYKYzaW9pucKZ07iy0NTBVFinSVjR_vDod53muK/exec";
+const URL_PLANILHA = "https://script.google.com/macros/s/AKfycbwjeHI4cEHzgQnMaSTDH6UioFGnGKWcwGotkKuL8sqkNzQRt4Vy9YgCAE9PTxnhfRla/exec";
