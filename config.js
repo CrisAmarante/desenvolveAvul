ea830dc328cab3c3ef1dc64d82e6ad7855e7d0aa
CRISTIANO DE AMARANTE SANTOS <cristiano.santos@urubupunga.com.br>
2026-08-11 12:19:44 -0300

    Limpeza do repositório

--- config.js
+++
@@ -1,8 +0,0 @@
-
-//=======================COMENTE SE NÃO ESTIVER EM USO====REMOVA O COMENTÁRIO SE FOR USAR=================================================
-//ambiente de desenvolvimento
-//const URL_PLANILHA = "URL app script.gs";
-
-//=======================COMENTE SE NÃO ESTIVER EM USO====REMOVA O COMENTÁRIO SE FOR USAR=================================================
-//ambiente de testes de campo
-const URL_PLANILHA = "https://script.google.com/macros/s/AKfycbyAIzgbB5WDx4G8HciCxMnIGNyndT6GJyBprKRUWGE8XM07jmArjYeiYea9ZqwNyc6e/exec";
