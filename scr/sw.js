9b2255cfd6c83abfb65e817424221711e01111e1
Cris Amarante <amarante_s@hotmail.com>
2026-08-12 07:29:13 -0300

    Update sw.js

--- scr/sw.js
+++ scr/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'penso-cache-v3.1.0.0.2.0';
+const CACHE_NAME = 'penso-cache-v3.1.0.0.2.1';
 //Inclusão do modal de Tacógrafos
 // Lista de arquivos para cache imediato (estáticos)
 const ASSETS_TO_CACHE = [
