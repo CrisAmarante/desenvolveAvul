9f94f7fc5e713bb045a80ee835239ee2104ddefb
Cris Amarante <amarante_s@hotmail.com>
2026-08-13 07:02:49 -0300

    Update CACHE_NAME version in service worker

--- scr/sw.js
+++ scr/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'penso-cache-v3.1.0.0.3.0';
+const CACHE_NAME = 'penso-cache-v3.1.0.0.3.1';
 //Mudança de BD
 // Lista de arquivos para cache imediato (estáticos)
 const ASSETS_TO_CACHE = [
